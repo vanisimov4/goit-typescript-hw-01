1fa7dc8e772ca35c5b72bd471ec1776200e81c83
Volodymyr Anisimov <144102084+vanisimov4@users.noreply.github.com>
2025-06-17 22:21:55 +0300

    initial

---
+++ src/basic/3.ts
@@ -0,0 +1,3 @@
+let mixedType: string | number;
+
+type EnableOrDisable = 'enable' | 'disable';
---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import './style.css';
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): string {
+  throw new Error('Error');
+}
+showMessage('t');
+console.log(calc(2, 3));
+
+console.log(customError());
+
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.ts'
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
